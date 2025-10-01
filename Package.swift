c6f6dc01ff95fcdf99f378f2defdcd8e3f0573ef
Boris Bielik <h3sperian@gmail.com>
2025-10-01 10:05:35 +0200

    Support Xcode 26

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0")
+    .package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"603.0.0")
   ],
   targets: [
     .macro(
